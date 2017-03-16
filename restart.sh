4b6484c53bb724f0046b142b595fba367f342ce6
Nadir Jeevanjee <Nadir.Jeevanjee@noaa.gov>
2017-03-16 14:19:06 -0400

    Modified restart.sh to identify nprev automatically

--- restart.sh
+++ restart.sh
@@ -7,28 +7,25 @@
 project=agg_co2
 damdir=~/$project/dam
 casedir=$1
-nprev=$2
 
-# Check that nprev specified
-if [ -z "${nprev}" ]; then
-   printf "No nprev specified"
-   exit
-fi 
+cd $casedir
+declare -i nprev=$( ls log* | cut -c4,5 | sort -n | tail -n1 )+1
+printf "nprev = ${nprev}"
 
 # Change filenames so data doesn't get overwritten
-for f in ${casedir}/data/*[st].nc; do
+for f in data/*[st].nc; do
     mv $f ${f%.nc}${nprev}.nc
 done
 
 #rename log, copy nml
-mv ${casedir}/log ${casedir}/log${nprev}
-cp ${casedir}/nml ${casedir}/nml${nprev}
+mv log log${nprev}
+cp nml nml${nprev}
 
 # Change ivdata_path in nml file
-cd $casedir
 sed -e "s|ivdata_path = .*|ivdata_path = \'${casedir}/data/snapshot${nprev}.nc\'|" \
    < ${damdir}/${casedir}/nml > ${damdir}/${casedir}/nml_post
 mv nml_post nml
 
 # Submit job
 msub submit 
+cd ..
